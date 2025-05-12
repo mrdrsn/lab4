656719a1903721c0ef1c63d068ad1f2c46f559a9
mrdrsn <nsokolova127@gmail.com>
2025-05-12 17:47:24 +0300

    final commit lab 4!!! w cloud bd

--- src/main/java/com/mycompany/lab4/DataBaseHandler.java
+++ src/main/java/com/mycompany/lab4/DataBaseHandler.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseHandler {
-    private static final String URL = "jdbc:postgresql://localhost:5432/Ollivanders";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "1";
+    private static final String URL = "jdbc:postgresql://aws-0-eu-north-1.pooler.supabase.com:5432/postgres";
+    private static final String USER = "postgres.gsrpnggfmkurksxjxvus";
+    private static final String PASSWORD = "parol";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
